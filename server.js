87f6143730af60b48cc0ce74f93bf3b1744e6de8
Glitch (truthful-mine) <none>
2018-05-03 17:01:03 +0000

    💣🚆 Checkpoint ./server.js:1010073/247

--- server.js
+++ server.js
@@ -18,16 +18,17 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   var reg =/(^[0-9.]+(?=,))/
-  var reg2 =/(?<=\().+?(?=\))/
+  //var reg2 =/(?<=\().+?(?=\))/
+  var b =req.rawHeaders[15].split("(")[1].split(")")[0]?req.rawHeaders[15].split("(")[1].split(")")[0]:req.rawHeaders[17].split("(")[1].split(")")[0]
   var a = req.rawHeaders[3].match(reg)
-  var b = req.rawHeaders[15].match(reg2)||req.rawHeaders[17].match(reg2)
+  //var b = req.rawHeaders[15].match(reg2)||req.rawHeaders[17].match(reg2)
  // var ipAddress = req.rawHeaders[3].split(",")[0]
   var language=req.rawHeaders[23].split(",")[0]
   if(language=="gzip"){
   language=req.rawHeaders[25].split(",")[0]
   }
   
-    res.send({ipAddress:a[0], software:b[0], language})
+    res.send({ipAddress:a[0], software:b, language})
     });
 
 // listen for requests :)
