cf5f2f135d0ceb3686fd7d65ef9b9a97fc9a1a6d
Glitch (truthful-mine) <none>
2018-05-03 06:16:49 +0000

    🤘🚇 Checkpoint ./server.js:1010073/98

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ var app = express();
 app.use(express.static('public'));
 //var reg =/(^[0-9.]+(?=,))/
 
-
+//15, 23 or 17, 25
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   var reg =/(^[0-9.]+(?=,))/
-  var reg2 =/\(.+\)/
+  var reg2 =/(?<=\().+?(?=\))/
   var a = req.rawHeaders[3].match(reg)
   var b = req.rawHeaders[15].match(reg2)
  // var ipAddress = req.rawHeaders[3].split(",")[0]
-  var language=req.rawHeaders[23].split(",")[0]
-    res.send({ipAddress:a[0], software:req.rawHeaders[15], language})
+  var language=req.rawHeaders[23].split(",")
+    res.send({ipAddress:a, software:req.rawHeaders, language})
     });
 
 // listen for requests :)
