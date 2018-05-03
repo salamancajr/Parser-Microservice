ae4f998fa7a4525c967b9d3a6a85be238f01c789
Glitch (truthful-mine) <none>
2018-05-03 05:35:49 +0000

    🐪👐 Checkpoint ./server.js:1010073/137

--- server.js
+++ server.js
@@ -5,18 +5,18 @@
 var express = require('express');
 var app = express();
  
-const bodyParser = require("body-parser")
+ 
  
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-app.use(bodyParser.json())
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   
-    res.send({ipAddress:req.rawHeaders, user:req.rawHeaders, language:req.rawHeaders[15]})
+    res.send({ipAddress:req.rawHeaders[3], software:req.rawHeaders[15], language:req.rawHeaders[23]})
     });
 
 // listen for requests :)
